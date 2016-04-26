9dec887f347138e4dd009740136c801d27ffc564
Fredrik Teschke <f@ftes.de>
2016-04-26 13:09:58 +0200

    add untrusted util functions

---
+++ sgx_lib_u/include/sgx_lib_u_util.h
@@ -0,0 +1,11 @@
+#ifndef SGX_LIB_U_UTIL_H
+#define SGX_LIB_U_UTIL_H
+
+#include <sgx_status.h>
+
+sgx_status_t check(sgx_status_t rc, char* error_msg);
+
+sgx_status_t launch_enclave(wchar_t* enclave_dll_file, sgx_enclave_id_t* eid);
+sgx_status_t destroy_enclave(sgx_enclave_id_t eid);
+
+#endif
---
+++ sgx_lib_u/sgx_lib_u_util.c
@@ -0,0 +1,28 @@
+#include <sgx_urts.h>
+#include <stdio.h>
+
+#include "sgx_lib.h"
+
+#include "sgx_lib_u_util.h"
+
+sgx_status_t check(sgx_status_t rc, char* error_msg) {
+  if (rc != SGX_SUCCESS) {
+    char* desc = get_error_description(rc);
+    fprintf(stderr, "%s (%s)\n", error_msg, desc);
+  }
+  return rc;
+}
+
+/* Very simple for now: debug mode, no launch token supported */
+ sgx_status_t launch_enclave(wchar_t* enclave_dll_file, /*out*/ sgx_enclave_id_t* eid) {
+  sgx_launch_token_t token = {0};
+  int updated = 0;
+
+  // Launch the enclave
+  // Token is not stored for now (would speed up subsequent launches)
+  return check(sgx_create_enclave(enclave_dll_file, SGX_DEBUG_FLAG, &token, &updated, eid, NULL), "Failed to launch enclave");
+}
+
+sgx_status_t destroy_enclave(sgx_enclave_id_t eid) {
+  return check(sgx_destroy_enclave(eid), "Failed to destroy enclave");
+}
